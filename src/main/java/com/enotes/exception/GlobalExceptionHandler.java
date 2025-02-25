657ad8ab01a340ee9fba887180d6c80cb74e433f
Vaibhav Borkar <101121602+Vaibhav-Borkar@users.noreply.github.com>
2025-02-25 17:22:51 +0530

    exist-category

--- src/main/java/com/enotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/exception/GlobalExceptionHandler.java
@@ -18,28 +18,36 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> handleGlobalException(Exception e) {
-		log.info("GlobalExceptionHandler :: handleGlobalException");
+		log.info("GlobalExceptionHandler :: GlobalExceptionHandler");
 		return new ResponseEntity<> (e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?> handleNullPointerException(Exception e){
-		log.info("NullPointerException :: handleNullPointerException");
+		log.info("NullPointerException :: NullPointerException");
 		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotFoundException(Exception e){
-		log.info("ResourceNotFoundException :: handleResourceNotFoundException");
+		log.info("ResourceNotFoundException :: ResourceNotFoundException");
 		return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
 	}
 	
 	
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<?> handleValidationException(ValidationException e){
-		log.info("ValidationException :: handleValidationException()");
+		log.info("ValidationException :: ValidationException");
 		return new ResponseEntity<>(e.getErrors(),HttpStatus.BAD_REQUEST);
 	}
+	
+	
+	@ExceptionHandler(DataExistsException.class)
+	public ResponseEntity<?> handleDataExistsException(DataExistsException e){
+		log.info("DataExistsException :: DataExistsException");
+		return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+	}
+	
 	
 }
