0e767e7a8348860da731541c666038bbd1759df6
Vaibhav Borkar <101121602+Vaibhav-Borkar@users.noreply.github.com>
2025-04-19 16:46:46 +0530

    swagger-implemented

--- src/main/java/com/enotes/auth/AuthEndpoint.java
+++ src/main/java/com/enotes/auth/AuthEndpoint.java
@@ -12,15 +12,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.enotes.user.UserRequest;
+import com.enotes.utils.SWAGGER;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Tag(name = "Authentication APIs",description = "All users authentication APIs like register and login")
 @RequestMapping("/api/v1/auth")
 public interface AuthEndpoint {
 
+	@Operation(summary = "Register User",description = SWAGGER.REGISTER_USER)
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@RequestBody UserRequest userDto,HttpServletRequest req) throws Exception;
 	
+	@Operation(summary = "Login User",description = SWAGGER.LOGIN_USER)
 	@PostMapping("/login")
 	public ResponseEntity<?> loginUserHandler(@RequestBody LoginRequest req);
 }
